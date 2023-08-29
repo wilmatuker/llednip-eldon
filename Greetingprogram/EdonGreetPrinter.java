f53749f3f17023b2fdfecbaab8f7e723168cb8bd
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-08-29 14:22:31 +0000

    last update to greeting thing

--- Greetingprogram/EdonGreetPrinter.java
+++ Greetingprogram/EdonGreetPrinter.java
@@ -3,10 +3,10 @@ class EdonGreetPrinter
     public static void main(String[] args) 
     {
         System.out.println("\nWILKOMMEN!  Ok done with the german");
-        System.out.print("User's Name: Edon Pindell");
-        System.out.print("\n");
+        System.out.print("User's Name: Edon Pindull");
+        System.out.println("");
         System.out.print("User's Age: 14");
-        System.out.print("\n");
+        System.out.println("");
         System.out.println("the german strikes again: Tchuß");
     }
 }
