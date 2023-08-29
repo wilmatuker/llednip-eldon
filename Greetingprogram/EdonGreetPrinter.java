cae93a1f36ecf21fc80ed035bf9de76188aa89a2
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-08-29 14:07:24 +0000

    another update to greeting program
    plus a proper helloworld program

--- Greetingprogram/EdonGreetPrinter.java
+++ Greetingprogram/EdonGreetPrinter.java
@@ -3,7 +3,7 @@ class EdonGreetPrinter
     public static void main(String[] args) 
     {
         System.out.println("\nWILKOMMEN!  Ok done with the german");
-        System.out.print("User's Name: Eldon Pindell");
+        System.out.print("User's Name: Edon Pindell");
         System.out.print("\n");
         System.out.print("User's Age: 14");
         System.out.print("\n");
