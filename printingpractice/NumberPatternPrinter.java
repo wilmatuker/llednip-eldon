a899790dee5b50acbeb6800378c4e8c7b8e148c8
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-08-31 13:21:15 +0000

    ok probably last time im updating this file

--- printingpractice/NumberPatternPrinter.java
+++ printingpractice/NumberPatternPrinter.java
@@ -1,5 +1,12 @@
 class NumberPatternPrinter {
     public static void main(String[] args) {
-        
+        System.out.println("\nWelcome to the number pattern printer\n");
+
+        System.out.println("1    ");
+        System.out.println("2 3   ");
+        System.out.println("4 5 6  ");
+        System.out.println("7 8 9 10\n");
+
+        System.out.println("Thank you for seeing this");
     }
 }
