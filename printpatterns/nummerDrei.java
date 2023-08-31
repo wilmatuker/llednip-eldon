047051fe3ea81bf0fc46e83234914e21314322f9
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-08-31 14:15:14 +0000

    Another file

---
+++ printpatterns/nummerDrei.java
@@ -0,0 +1,18 @@
+/* 
+ *Pardon the german
+ *
+ * English: Number three
+ */
+class nummerDrei {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
