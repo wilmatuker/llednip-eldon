462e0d2e363572c0aab6210ce87fe2c57fca8494
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-08-31 13:43:35 +0000

    goofy text art

---
+++ text art/goofyTextArt.java
@@ -0,0 +1,17 @@
+class goofyTextArt {
+    public static void main(String[] args) {
+        System.out.println("modern art\n");
+        
+        System.out.println("                       ___     ");
+        System.out.println("                     /    /      ");
+        System.out.println("                    /    /       ");
+        System.out.println("                   /    /        ");
+        System.out.println("                  |    |         ");
+        System.out.println("                 \\    \\           ");
+        System.out.println("                   \\   \\         ");
+        System.out.println("                     \\  \\         ");
+        System.out.println("                        __         ");
+
+        System.out.println("\nThank you for viewing this work of art");
+    }    
+}
