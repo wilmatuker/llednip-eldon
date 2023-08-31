4fec90af8ccb5765eb0aa516f3a7e708253bb211
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-08-31 14:07:35 +0000

    why did a make this

---
+++ shortStory/shortStory.java
@@ -0,0 +1,11 @@
+
+public class shortStory {
+    public static void main(String[] args) {
+        System.out.println("The moth\n");
+        System.out.println("Once upon a time there was a moth.\n");
+        System.out.println("This moth was snoozing on a door.\n");
+        System.out.println("Then somebody was getting less than six feet away.\n");
+        System.out.println("Then the moth decided to jumpscare the person.\n");
+        System.out.println("Thank you for reading this");
+    }    
+}
