dfaf68f9af4dfb257f36d4e6e922d66ce34ef2ad
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-08-31 14:17:48 +0000

    the file is done

---
+++ printpatterns/nummerZwei.java
@@ -0,0 +1,14 @@
+/*
+ * Pardon the german
+ * 
+ * English: Number Two
+ */
+class nummerZwei {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }    
+}
