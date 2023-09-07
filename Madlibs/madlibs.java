ef2c7faa6f7def0f99cd9cdf2a7683f5ef3f73d1
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-07 13:22:33 +0000

    the madlibs v1

--- Madlibs/madlibs.java
+++ Madlibs/madlibs.java
@@ -1,6 +1,40 @@
 public class madlibs {
     public static void main(String args[])
     {
-        System.out.print("");
+        String name1 = "";
+        String title = "";
+        String animal = "";
+        String name2 = "";
+        String timeOfDay = "";
+        String dayOfTheWeek = "";
+        String verb = "";
+        String place1 = ""; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println("Once upon a time there was a " + title + " named " + name1 + ".");
+        System.out.println("He had a pet " + animal + " named " + name2 + ".");
+        System.out.println("Every " + timeOfDay + " on " + dayOfTheWeek + ", " + name1 + " would take " + "name2" + " for " + verb + "s by the" + place1 + ".");
+        System.out.println("The end.");
     }    
 }
