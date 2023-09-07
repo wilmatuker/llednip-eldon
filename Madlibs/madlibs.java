0e45e2fe306fa14cde6a0572ec51029348d5ea18
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-07 13:28:50 +0000

    hopefully final version (v2)

--- Madlibs/madlibs.java
+++ Madlibs/madlibs.java
@@ -1,14 +1,19 @@
 public class madlibs {
     public static void main(String args[])
     {
+        /*
+         * -------------------------------------------------------------------
+         * Be sure to fill this out without looking at the rest of the program
+         * -------------------------------------------------------------------
+        */
         String name1 = "";
         String title = "";
         String animal = "";
         String name2 = "";
         String timeOfDay = "";
         String dayOfTheWeek = "";
-        String verb = "";
-        String place1 = ""; 
+        String presentTenseVerb = "";
+        String place = ""; 
 
 
 
@@ -34,7 +39,7 @@ public class madlibs {
 
         System.out.println("Once upon a time there was a " + title + " named " + name1 + ".");
         System.out.println("He had a pet " + animal + " named " + name2 + ".");
-        System.out.println("Every " + timeOfDay + " on " + dayOfTheWeek + ", " + name1 + " would take " + "name2" + " for " + verb + "s by the" + place1 + ".");
+        System.out.println("Every " + timeOfDay + " on " + dayOfTheWeek + ", " + name1 + " would take " + name2 + " for " + presentTenseVerb + "s by the " + place + ".");
         System.out.println("The end.");
     }    
 }
