b795a4e81cc0bbb0cb0bf0c932958fad85a3dd26
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-14 14:05:54 +0000

    average calc

---
+++ miniMath/average6.java
@@ -0,0 +1,10 @@
+class average6 {
+    public static void main(String[] args) {
+        //change these to your three numbers
+        double num1 = 6.8;
+        double num2 = 4;
+        double num3 = 2.4;
+
+        System.out.println("The average is: " + ((num1 + num2 + num3) / 3));
+    }    
+}
