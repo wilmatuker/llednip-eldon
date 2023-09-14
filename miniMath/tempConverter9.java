1562e4dae1d9453e608fb44dd288d0be5b52d3dc
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-14 14:25:26 +0000

    temp calculator

---
+++ miniMath/tempConverter9.java
@@ -0,0 +1,8 @@
+class tempconverter9 {
+    public static void main(String[] args) {
+        //change to the temp
+        double farenheit = -40;
+
+        System.out.println((farenheit - 32) * (5.0/9.0));
+    }    
+}
