ccc01b2551c7da1100346613f18f216c0aebdc3c
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-14 13:17:57 +0000

    new folder + new program

---
+++ miniMath/add1.java
@@ -0,0 +1,9 @@
+public class add1 {
+    public static void main(String[] args) {
+        //change these to do addition
+        double num1 = 10.8;
+        double num2 = 9;
+
+        System.out.println("The answer is: " + (num1 + num2));
+    }    
+}
