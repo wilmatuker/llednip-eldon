4357f4400fd2fbdc2df66eac39252408467e6913
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-14 13:52:44 +0000

    hopefully final version

---
+++ miniMath/trapazoidPeremeter4.java
@@ -0,0 +1,11 @@
+class trapazoidPeremeter4 {
+    public static void main(String[] args) {
+        //change these to each side
+        double side1;
+        double side2;
+        double side3;
+        double side4;
+
+        System.out.println("The peremeter is: " + (side1 + side2 + side3 + side4));
+    }    
+}
