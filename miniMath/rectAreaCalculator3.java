f14dc70c3f92dd85a88fb062f69eada29da70716
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-14 13:46:36 +0000

    hopefully final area calculator version

--- miniMath/rectAreaCalculator3.java
+++ miniMath/rectAreaCalculator3.java
@@ -1,5 +1,10 @@
 class rectAreaCalculator3 {
     public static void main(String[] args) {
+        //change this to the length
+        double length = 78;
+        //change this to the width
+        double width = 32;
         
+        System.out.println("The area is:" + (length * width));
     }
 }
