73450fedf41442cdc18d23f0f676fd66b2da7228
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-14 13:58:25 +0000

    discount calculator

---
+++ miniMath/discountCalc5.java
@@ -0,0 +1,10 @@
+class discountCalc5 {
+    public static void main(String[] args) {
+        //change to original price
+        double originalPrice = 0.0;
+        //change to discount (percent in decimal form)
+        double discount = 0.0;
+        
+        System.out.println("Discounted price is: " + originalPrice * (1.0 - discount));
+    }  
+}
