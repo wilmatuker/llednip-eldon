6faae910d5c58e76bc30c0236412544bd136eba5
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-14 13:27:09 +0000

    new program + smol changes

--- miniMath/add1.java
+++ miniMath/add1.java
@@ -1,8 +1,8 @@
 class add1 {
     public static void main(String[] args) {
         //change these to do addition
-        double num1 = 10.8;
-        double num2 = 9;
+        double num1;
+        double num2;
 
         System.out.println("The answer is: " + (num1 + num2));
     }    
--- miniMath/birthday2.java
+++ miniMath/birthday2.java
@@ -1,5 +1,16 @@
 class birthday2 {
     public static void main(String[] args) {
-        
+        /*
+         * change this to your age for fun and profit
+         * 
+         * or to run the program
+        */
+        int age = 15;
+        /*
+         * ditto but change it to the current year
+        */
+        int currentYear = 2023;
+
+        System.out.println("Your birth year is: " + (currentYear - age));
     }
 }
