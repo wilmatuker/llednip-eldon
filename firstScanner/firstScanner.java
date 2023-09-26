312b07f8023878c32c45b543fbe606b3876dcae0
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-26 14:21:08 +0000

    first scanner program

---
+++ firstScanner/firstScanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class firstScanner {
+    public static void main(String[] args) {
+        Scanner skanner = new Scanner(System.in);
+        System.out.println("Enter the amount of revolutions around the sun you've existed for");
+        System.out.println("\n(Just enter thine age)\n");
+        System.out.println("Enter here: ");
+        int age = skanner.nextInt();
+        //change age to char because ascii exists
+        char ageChar = (char) (age + 32);
+        System.out.println("The ascii character coresponding to your age plus 32: " + (ageChar));
+        System.out.println("(Some characters are control characters)");
+    }
+}
