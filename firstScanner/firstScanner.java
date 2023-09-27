0662754366d161549339fc417f86826912521994
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-27 14:32:16 +0000

    scanner program

--- firstScanner/firstScanner.java
+++ firstScanner/firstScanner.java
@@ -7,9 +7,11 @@ public class firstScanner {
         System.out.println("\n(Just enter thine age)\n");
         System.out.println("Enter here: ");
         int age = skanner.nextInt();
+        //division
+        age /= 2
         //change age to char because ascii exists
         char ageChar = (char) (age + 32);
-        System.out.println("The ascii character coresponding to your age plus 32: " + (ageChar));
+        System.out.println("The ascii character coresponding to half your age plus 32: " + (ageChar));
         System.out.println("(Some characters are control characters)");
     }
 }
