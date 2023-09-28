588d4abc06006171d9a76c5841e1c9f0eec4a04e
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-09-28 14:20:47 +0000

    start celsius thing

---
+++ miniScannerProblems/celsius.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class celsius {
+    public static void main(String[] args) {
+        Scanner skinair = new Scanner(System.in);
+        System.out.println("You never learned Farenheit, so you use this program to make it make sense to your brain (makes sense)");
+        System.out.println("What is the number before the degrees F you see?");
+        double f = skinair.nextDouble();
+    }
+}
