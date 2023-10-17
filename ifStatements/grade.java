bed916200128456d1180842119cf70735daa24d2
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-10-17 14:03:19 +0000

    final grade commit i hope

--- ifStatements/grade.java
+++ ifStatements/grade.java
@@ -6,6 +6,23 @@ class grade {
 
         System.out.println("You have an exam and its graded  with a number, but you wanna know the letter grade");
         System.out.println("What is the number grade (0-100)");
+        numGrade = skooner.nextDouble() / 100;
+
+        if (numGrade > 1) {
+            System.out.println("Follow instructions please");
+        } else if (numGrade >= .9) {
+            System.out.println("That's an A!");
+        } else if (numGrade >= .89) {
+            System.out.println("That's a B");
+        } else if (numGrade >= .70) {
+            System.out.println("That's a C");
+        } else if (numGrade >= .59) {
+            System.out.println("That's a D");
+        } else if (numGrade >= .58) {
+            System.out.println("That's an F :(");
+        } else {
+            System.out.println("wat ._.");
+        }
 
         
     }    
