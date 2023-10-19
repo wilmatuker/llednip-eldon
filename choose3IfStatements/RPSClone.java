f37b1931384c2bcfa4dc3dcc387697ccb2223316
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-10-19 14:28:42 +0000

    Start rock paper scissors clone program

---
+++ choose3IfStatements/RPSClone.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.random;
+
+class RPSClone{
+    public static void main(String[] args) {
+        Random cpu;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hi today it's time for a rock paper scissors game with whoever wins 2 out of 3 matches is the victor");
+
+        System.out.println("Chose from rock, paper, or scissors");
+        
+    }
+}
