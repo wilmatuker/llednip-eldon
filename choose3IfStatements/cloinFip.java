9c08e410e42690d1d9b02d87535c115b82a0944c
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-10-25 15:30:56 +0000

    better cloin update

--- choose3IfStatements/cloinFip.java
+++ choose3IfStatements/cloinFip.java
@@ -1,13 +1,12 @@
 import java.util.Random;
-import java.util.random;
 
 class cloinFip {
     public static void main(String[] args) {
         Random coin = new Random();
 
-        int fip = coin.nextInt(0);
+        int fip = coin.nextInt(2);
 
-        if (fip == 0) {
+        if (fip == 1) {
             System.out.println("Heads");
         }
         else {
