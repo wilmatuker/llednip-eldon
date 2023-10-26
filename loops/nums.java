8c5ded47cfbcf998fbd85e2c5993911da1a73931
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-10-26 13:26:03 +0000

    making it make sense more

--- loops/nums.java
+++ loops/nums.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 class nums {
     public static void main(String[] args) {
-        double nums[] = {Double.NaN}
+        double[] nums = {Double.NaN}
         Scanner skannear = new Scanner(System.in);
         System.out.println("How many numbers do you want to put in? (positive answers only)");
         int ans = skannear.nextInt();
         
 
         if (ans > 0){
-            double nums[] = new double[ans];
+            nums = new double[ans];
             System.out.println(nums.length);
         }
         
