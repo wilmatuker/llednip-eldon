bb06da680a5452f2dc2042b264cd2b2d39ad3133
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-10-26 13:25:12 +0000

    number list (unfinished)

--- loops/nums.java
+++ loops/nums.java
@@ -2,24 +2,19 @@ import java.util.Scanner;
 
 class nums {
     public static void main(String[] args) {
+        double nums[] = {Double.NaN}
         Scanner skannear = new Scanner(System.in);
-        double[] nums = new double[1];
-        System.out.println("Please enter a number");
-        nums[0] = skannear.nextDouble();
-        while (skannear.next().toLowerCase() != "end") {
-            if (skannear.next().toLowerCase() == "end") {
-                if (nums.length == 1) {
-                    System.out.println("There was only one number: " + String.valueOf(nums[0]));
-                }
-            }
-            else {
-                System.out.println("Please enter a number, or \"end\" if you want to stop");
-                double num = skannear.nextDouble();
-                int end = nums.length;
-                nums = new double[nums.length + 1];
-                nums[end] = num;
-            }
+        System.out.println("How many numbers do you want to put in? (positive answers only)");
+        int ans = skannear.nextInt();
+        
+
+        if (ans > 0){
+            double nums[] = new double[ans];
+            System.out.println(nums.length);
         }
-        System.out.println(nums);
+        
+        if (nums[0] != Double.NaN) {
+            System.out.println("Enter a number");
+        }
     }    
 }
