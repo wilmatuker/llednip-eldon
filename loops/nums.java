107d465f4d42c50172417853e625ae8cb2a02994
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-10-26 13:46:40 +0000

    ITS DONE YAY

--- loops/nums.java
+++ loops/nums.java
@@ -2,19 +2,32 @@ import java.util.Scanner;
 
 class nums {
     public static void main(String[] args) {
-        double[] nums = {Double.NaN}
         Scanner skannear = new Scanner(System.in);
-        System.out.println("How many numbers do you want to put in? (positive answers only)");
-        int ans = skannear.nextInt();
-        
-
-        if (ans > 0){
-            nums = new double[ans];
-            System.out.println(nums.length);
-        }
-        
-        if (nums[0] != Double.NaN) {
-            System.out.println("Enter a number");
+        long greatest = 0;
+        long smallest = 0;
+        System.out.println("How many numbers do you want to put in? (positive answers only above 0)");
+        long maxNums = skannear.nextLong();
+        if (maxNums > 0) {
+            for (int i = 0; i <= maxNums; i++) {
+                if (i <= maxNums) {
+                    System.out.println("Enter a number");
+                    long num = skannear.nextLong();
+                    if (num > greatest) {
+                        greatest = num;
+                    } else if (num < smallest) {
+                        smallest = num;
+                    }
+                }
+            }
+            if (greatest == smallest) {
+                System.out.println("Both the greatest and smallest were the same number:");
+                System.out.println(greatest);
+            } else {
+                System.out.println("The greatest was: " + String.valueOf(greatest));
+                System.out.println("The smallest was: " + String.valueOf(smallest));
+            }
+        } else {
+            System.out.println("Why\nno");
         }
     }    
 }
