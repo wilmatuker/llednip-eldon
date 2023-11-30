9932be1ea599522cb1f34b56f6093266c10e7041
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-11-30 15:17:59 +0000

    saving it yet again

--- ArrayReverse/revArray.java
+++ ArrayReverse/revArray.java
@@ -1,27 +1,36 @@
 import java.util.Scanner;
 
 class revArray {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner amput = new Scanner(System.in);
         System.out.println("How big should the array be?");
         int size = amput.nextInt();//Size of array
+        amput.nextLine();
+
         String[] array = new String[size];
+
         //add values to array
         for(int i = 0; i < size; i++) {
            System.out.print("Enter value\n>> ");
            array[i] = amput.nextLine();
         }
-        for (int i = array.length-1; i > 0; i++) {
+
+        for (int i = array.length-1; i > 0; i--) 
+        {
             var temp = array[i];//Temporary value storage
-            for (int j = array.length-1; j > 0; j++) {
-                if (j + 1 != array.length) {
-                    array[j + 1] = array[j];//Shift all values up one now that we have the last one stored
-                }
+            System.out.println("array:");
+            for (int j2 = 0; j2 < array.length; j2++) {
+                System.out.println(array[j2]);
             }
+            
+            array[i] = array[0];
             array[0] = temp;
         }
+
         String arrayString = "[";
-        for (int i = 0; i < size; i++) {
+
+        for (int i = 0; i < array.length; i++) {
             arrayString += "\"";
             arrayString += array[i];
             arrayString += "\"";
@@ -30,7 +39,7 @@ class revArray {
             }
         }
         arrayString += "]";
-        System.out.println("The array: " + array);
+        System.out.println("The array: " + arrayString);
 
     }    
 }
