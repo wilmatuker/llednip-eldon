ed32fa1a5fd4773a03e4f925073dc5a07174a3cc
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2023-12-13 16:06:39 +0000

    wip 4th problem

---
+++ arrayProblems/problem4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class problem4 {
+    public static void main(String args[]) {
+        Scanner impuuuuut = new Scanner(System.in);
+        System.out.println("How many numbers do you want to find the min of?");
+        int size = impuuuuut.nextInt();
+        double[] numbers = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("What number shall thee add to the compendium of numerical values?");
+
+        }
+    }
+}
