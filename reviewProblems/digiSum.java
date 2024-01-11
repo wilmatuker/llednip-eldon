eccc04a1e97206175b6452f9aa51863c438aa61f
wilmatuker <100317891+wilmatuker@users.noreply.github.com>
2024-01-11 14:59:35 +0000

    digit addition shenanigans

--- reviewProblems/digiSum.java
+++ reviewProblems/digiSum.java
@@ -5,8 +5,30 @@ class digiSum{
         Scanner inpuuut = new Scanner(System.in);
         
         System.out.print("Type in any number please \n    >> ");
-        double number;
+        double number = inpuuut.nextDouble();
+        long numint = 0;
+
+        boolean isInteger = false;
+        if (number % 1 == 0){
+            numint = Math.round(number);
+            isInteger = true;
+        }
 
         int sum = 0;
+        String digitString = "";
+
+
+        if (isInteger == true) {
+            digitString = String.valueOf(numint);
+        }
+        else {
+            digitString = String.valueOf(number);
+        }
+
+        for (int c = 0; c < digitString.length(); c++) {
+            sum += digitString.charAt(c) - 48;
+        }
+
+        System.out.println("The digits added together make " + sum);
     }
 }
